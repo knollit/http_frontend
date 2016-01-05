86ecd99109ce632cdb72a9c2c88322ea1c7bb9d6
Michael Raimondi <mike@mikeraimondi.com>
2016-01-04 21:23:53 -0500

    Change flatc output in CI

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-flatc -g *.fbs
+if [ "$CIRCLECI" = true ]
+then
+  flatc -g -o ~/.go_workspace/src/github.com/knollit/$CIRCLE_PROJECT_REPONAME/ *.fbs
+else
+  flatc -g *.fbs
+fi
 go get
 CGO_ENABLED=0 GOOS=linux go build -a --installsuffix cgo --ldflags="-s" -o http_frontend .
 docker build -t http_frontend:latest .
