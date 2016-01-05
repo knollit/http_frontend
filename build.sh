4796164ba06f91f7a800390112d15d140f165ba5
Michael Raimondi <mike@mikeraimondi.com>
2016-01-04 21:34:44 -0500

    Change image name

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ else
 fi
 go get
 CGO_ENABLED=0 GOOS=linux go build -a --installsuffix cgo --ldflags="-s" -o http_frontend .
-docker build -t http_frontend:latest .
+docker build -t knollit/http_frontend:latest .
 rm http_frontend
