0deafff4e899a4e8f731c97709ef2bab12567d90
Michael Raimondi <mike@mikeraimondi.com>
2016-02-09 20:40:32 -0500

    Extract support function to library

--- e2e_test.go
+++ e2e_test.go
@@ -7,93 +7,14 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"os"
-	"os/exec"
 	"testing"
-	"time"
+
+	"github.com/mikeraimondi/go_compose"
 )
 
+var context compose.TestContext
+
 const port = ":6080"
-
-type e2eContext struct {
-	built   bool
-	ip      []byte
-	logFile *os.File
-	testNum int
-}
-
-var context e2eContext
-
-func e2e(t *testing.T, testFunc func([]byte)) {
-	if testing.Short() {
-		t.Skip("skipping end-to-end test in short mode.")
-	}
-
-	// build project if unbuilt
-	if !context.built {
-		if err := exec.Command("./build.sh").Run(); err != nil {
-			t.Fatal("build failed: ", err)
-		}
-		context.built = true
-	}
-
-	// get Docker IP and cache it
-	if len(context.ip) == 0 {
-		dkm, err := exec.Command("docker-machine", "active").Output()
-		if err == nil { // active Docker Machine detected, use it
-			byteIP, err := exec.Command("docker-machine", "ip", string(bytes.TrimSpace(dkm))).Output()
-			if err != nil {
-				t.Fatal(err)
-			}
-			context.ip = bytes.TrimSpace(byteIP)
-		} else { // no active docker machine, assume Docker is running natively
-			context.ip = []byte("127.0.0.1")
-		}
-	}
-
-	// bring up Compose
-	if err := exec.Command("docker-compose", "up", "-d").Run(); err != nil {
-		t.Fatal("Docker compose failed to start: ", err)
-	}
-	defer func() {
-		if err := exec.Command("docker-compose", "down").Run(); err != nil {
-			t.Fatal(err)
-		}
-	}()
-
-	// log Compose output
-	// TODO timestamps
-	if context.logFile == nil {
-		context.logFile, _ = os.Create("test.log")
-		cmd := exec.Command("docker-compose", "logs", "--no-color")
-		cmd.Stdout = context.logFile
-		cmd.Stderr = context.logFile
-		if err := cmd.Start(); err != nil {
-			t.Fatal(err)
-		}
-	}
-	context.testNum++
-	context.logFile.WriteString(fmt.Sprintf("--- test %v start\n", context.testNum))
-	defer context.logFile.WriteString(fmt.Sprintf("--- test %v end\n", context.testNum))
-
-	// poll until server is healthy
-	start := time.Now()
-	for func() bool {
-		resp, err := http.Head(fmt.Sprintf("http://%s%v/health_check", context.ip, port))
-		if err == nil && resp.StatusCode == 204 {
-			return false
-		}
-		return true
-	}() {
-		if time.Now().Sub(start) > time.Second*30 {
-			t.Fatal("timed out waiting for server to start.")
-		}
-		time.Sleep(time.Millisecond * 250)
-	}
-
-	// Run test
-	testFunc(context.ip)
-}
 
 func assertGet(t *testing.T, url string, expected interface{}) {
 	resp, err := http.Get(url)
@@ -111,7 +32,7 @@ func assertGet(t *testing.T, url string, expected interface{}) {
 }
 
 func TestOrganizationIndexE2E(t *testing.T) {
-	e2e(t, func(ip []byte) {
+	compose.RunTest(t, &context, port, func(ip []byte) {
 		orgURL := fmt.Sprintf("http://%s%v/organizations", ip, port)
 
 		assertGet(t, orgURL, []organization{})
